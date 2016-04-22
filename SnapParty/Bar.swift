6ca84fbbdd2d1b77c79c567837c743f0438f8f15
summerke <robin.welten@planet.nl>
2016-04-22 11:10:22 +0200

    Added files via upload
    bar

---
+++ SnapParty/Bar.swift
@@ -0,0 +1,26 @@
+//
+//  bar.swift
+//  SnapParty
+//
+//  Created by Fhict on 22/04/16.
+//  Copyright © 2016 Fhict. All rights reserved.
+//
+
+import Foundation
+
+class Bar
+{
+    var name:String
+    //var licenseholder :String
+    var longitude :String
+    var latitude :String
+    
+    
+    init(name: String, latitude: String, longitude: String)
+    {
+        self.name = name
+        self.longitude = longitude
+        self.latitude = latitude
+        
+    }
+}
